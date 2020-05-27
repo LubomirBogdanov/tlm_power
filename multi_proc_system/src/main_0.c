fd3b996e74084e24fb0143260a73f196eb302f88
Lubomir Bogdanov <lbogdanov@tu-sofia.bg>
2020-05-27 22:33:02 +0300

    i2c: using only registers now. I can send start, address and stop.
    delay: made a blocking delay in ms with the help of the systick.

--- multi_proc_system/src/main_0.c
+++ multi_proc_system/src/main_0.c
@@ -14,7 +14,16 @@ void SysTick_Handler(void){
 	ticks++;
 }
 
+void delay_ms(uint16_t delay_value){
+	uint32_t ticks_timeout = ticks;
+	ticks_timeout += delay_value;
+	while(ticks < ticks_timeout){
+		__WFI();
+	}
+}
+
 int main(void){
+	uint32_t ret = 0;
 	uint8_t txData[16];
 	uint8_t rxData[16];
 	I2CM_XFER_T i2cmXferRec;
@@ -27,26 +36,45 @@ int main(void){
 	i2cmXferRec.rxBuff = rxData;
 
 	SystemCoreClockUpdate();
-	Chip_GPIO_Init(LPC_GPIO_PORT);
 
-	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_SWM);
-	Chip_SWM_MovablePinAssign(SWM_I2C_SDA_IO, 0);
-	Chip_SWM_MovablePinAssign(SWM_I2C_SCL_IO, 1);
-	Chip_IOCON_PinSetI2CMode(LPC_IOCON, IOCON_PIO0, PIN_I2CMODE_FASTPLUS);
-	Chip_IOCON_PinSetI2CMode(LPC_IOCON, IOCON_PIO1, PIN_I2CMODE_FASTPLUS);
-	Chip_Clock_DisablePeriphClock(SYSCTL_CLOCK_SWM);
+	LPC_SYSCTL->SYSAHBCLKCTRL |= 1 << SYSCTL_CLOCK_SWM;
+	LPC_SYSCTL->SYSAHBCLKCTRL |= 1 << SYSCTL_CLOCK_GPIO;
+	LPC_SYSCTL->SYSAHBCLKCTRL |= 1 << SYSCTL_CLOCK_IOCON;
+	LPC_SYSCTL->SYSAHBCLKCTRL |= 1 << SYSCTL_CLOCK_I2C0;
 
-	Chip_I2C_Init(LPC_I2C);
-	Chip_I2C_SetClockDiv(LPC_I2C, 40);
-	Chip_I2CM_SetBusSpeed(LPC_I2C, 100000);
-	Chip_I2CM_Enable(LPC_I2C);
+	LPC_SWM->PINASSIGN[7] &= 0x00FFFFFF;
+	LPC_SWM->PINASSIGN[8] &= 0xFFFFFF00;
+	LPC_SWM->PINASSIGN[7] |= 0x01 << 24; //P0.1 = I2C_SDA
+	LPC_SWM->PINASSIGN[8] |= 0x00; //P0.0 = I2C_SCL
+	LPC_IOCON->PIO0[0] |= PIN_OD_MASK;
+	LPC_IOCON->PIO0[1] |= PIN_OD_MASK;
+
+	LPC_SYSCON->PRESETCTRL &= ~(1 << 6);
+	LPC_SYSCON->PRESETCTRL |= 1 << 6;
+
+	LPC_I2C->CLKDIV = 20;
+	LPC_I2C->MSTTIME = 0x01; //3 clocks SCL(high), 2 clocks SCL(low), do not use logic operators here
+	LPC_I2C->CFG |= 0x01; //Enable master
 
 	NVIC_DisableIRQ(I2C_IRQn);
 	SysTick_Config(SystemCoreClock / TICKRATE_HZ);
 
 	while(1) {
-		Chip_I2CM_XferBlocking(LPC_I2C, &i2cmXferRec);
-		__WFI();
+		//Chip_I2CM_XferBlocking(LPC_I2C, &i2cmXferRec);
+
+		LPC_I2C->STAT |= (I2C_STAT_MSTRARBLOSS | I2C_STAT_MSTSTSTPERR);
+		LPC_I2C->MSTDAT = 0xC0; //Write to 0x60 7-bit
+		LPC_I2C->MSTCTL |= I2C_MSTCTL_MSTSTART; //Start
+
+		//while (ret == 0) {
+			/* wait for status change interrupt */
+			while(!(LPC_I2C->STAT & I2C_STAT_MSTPENDING)) { }
+			/* call state change handler */
+			//ret = Chip_I2CM_XferHandler(LPC_I2C, &i2cmXferRec);
+			LPC_I2C->MSTCTL |= I2C_MSTCTL_MSTSTOP; //Stop
+		//}
+
+		delay_ms(1);
 	}
 }
 
