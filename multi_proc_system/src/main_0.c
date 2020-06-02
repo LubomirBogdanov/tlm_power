a5e547d02e0e42374412faa31435108dd3dd2c08
Lubomir Bogdanov <lbogdanov@tu-sofia.bg>
2020-06-02 23:06:13 +0300

    i2c: master read is done. I'm using AT24C256 eeprom to test my code.

--- multi_proc_system/src/main_0.c
+++ multi_proc_system/src/main_0.c
@@ -46,6 +46,10 @@ void user_i2c_enable_master(void){
 	LPC_I2C->CFG |= 0x01; //Enable master
 }
 
+void user_i2c_clear_status(void){
+	LPC_I2C->STAT = (I2C_STAT_MSTRARBLOSS | I2C_STAT_MSTSTSTPERR);
+}
+
 /*!
  * \brief
  *
@@ -55,23 +59,39 @@ void user_i2c_enable_master(void){
 uint8_t user_i2c_master_ack_get(void){
 	uint8_t master_state = 0;
 	uint8_t slave_ack = 0;
+	uint32_t master_status = 0;
 
-	while(!(LPC_I2C->STAT & I2C_STAT_MSTPENDING)) { } //Wait while sending address
+	while(!(LPC_I2C->STAT & I2C_STAT_MSTPENDING)) { }
 
-	master_state = (LPC_I2C->STAT & I2C_STAT_MSTSTATE) >> 1;
+	master_status = LPC_I2C->STAT;
 
-	if(master_state == I2C_STAT_MSTCODE_TXREADY){
-		slave_ack = 1;
-	}
-	else if(master_state == I2C_STAT_MSTCODE_IDLE){
+	if(master_status & I2C_STAT_MSTRARBLOSS){
 
 	}
-	else if(master_state == I2C_STAT_MSTCODE_NACKADR){
+	else if(master_status & I2C_STAT_MSTSTSTPERR){
 
 	}
-	else if(master_state == I2C_STAT_MSTCODE_NACKDAT){
+	else{
+		master_state = (LPC_I2C->STAT & I2C_STAT_MSTSTATE) >> 1;
 
+		if(master_state == I2C_STAT_MSTCODE_TXREADY){
+			slave_ack = 1;
+		}
+		else if(master_state == I2C_STAT_MSTCODE_RXREADY){
+			slave_ack = 1;
+		}
+		/*else if(master_state == I2C_STAT_MSTCODE_IDLE){
+
+		}
+		else if(master_state == I2C_STAT_MSTCODE_NACKADR){
+
+		}
+		else if(master_state == I2C_STAT_MSTCODE_NACKDAT){
+
+		}*/
 	}
+
+	user_i2c_clear_status();
 
 	return slave_ack;
 }
@@ -85,12 +105,11 @@ uint8_t user_i2c_master_ack_get(void){
  *
  * \return 0 - transmission was successful, 1 - transmission was incomplete (NACK was detected)
  */
-uint8_t user_i2c_master_write(uint8_t slave_addr,  uint16_t slave_data_len, uint8_t *slave_data){
+uint8_t user_i2c_master_write(uint8_t slave_addr, uint16_t slave_data_len, uint8_t *slave_data){
 	uint8_t err = 0;
 	uint16_t i;
 
-	//	LPC_I2C->STAT |= (I2C_STAT_MSTRARBLOSS | I2C_STAT_MSTSTSTPERR);
-	LPC_I2C->STAT = (I2C_STAT_MSTRARBLOSS | I2C_STAT_MSTSTSTPERR);
+	user_i2c_clear_status();
 
 	LPC_I2C->MSTDAT = slave_addr << 1; //Make 8-bit address
 
@@ -104,6 +123,36 @@ uint8_t user_i2c_master_write(uint8_t slave_addr,  uint16_t slave_data_len, uint
 				err = 1;
 				break;
 			}
+		}
+	}
+
+	LPC_I2C->MSTCTL = I2C_MSTCTL_MSTSTOP; //Stop
+
+	return err;
+}
+
+uint8_t user_i2c_master_read(uint8_t slave_addr, uint16_t slave_data_len, uint8_t *slave_data){
+	uint8_t err = 0;
+	uint16_t i;
+
+	user_i2c_clear_status();
+
+	LPC_I2C->MSTDAT = (slave_addr << 1) | 0x01; //Make 8-bit address
+
+	LPC_I2C->MSTCTL = I2C_MSTCTL_MSTSTART; //Start
+
+	if(user_i2c_master_ack_get()){
+		slave_data[0] = LPC_I2C->MSTDAT;
+	}
+
+	for(i = 1; i < slave_data_len; i++){
+		LPC_I2C->MSTCTL = I2C_MSTCTL_MSTCONTINUE;
+		if(user_i2c_master_ack_get()){
+			slave_data[i] = LPC_I2C->MSTDAT;
+		}
+		else{
+			err = 1;
+			break;
 		}
 	}
 
@@ -126,7 +175,9 @@ void user_gpio_clear(void){
 }
 
 int main(void){
-	uint8_t slv_data[3] = {0x01, 0x33, 0x55};
+	uint8_t i;
+	uint8_t slv_data_tx[3] = {0x01, 0x33, 0x55};
+	uint8_t slv_data_rx[4] = {0x00, 0x00, 0x00, 0x00};
 
 	SystemCoreClockUpdate();
 
@@ -137,13 +188,20 @@ int main(void){
 	NVIC_DisableIRQ(I2C_IRQn);
 	SysTick_Config(SystemCoreClock / TICKRATE_HZ);
 
+	user_i2c_master_write(I2C_ADDR_7BIT, 3, slv_data_tx);
+	delay_ms(100);
+
 	while(1) {
-		if(user_i2c_master_write(I2C_ADDR_7BIT, 3, slv_data)){
+		user_i2c_master_write(I2C_ADDR_7BIT, 2, slv_data_tx);
+		user_i2c_master_read(I2C_ADDR_7BIT, 4, slv_data_rx);
+
+		if(slv_data_tx[2] != slv_data_rx[0]){
 			user_gpio_set();
 		}
 		else{
 			user_gpio_clear();
 		}
+
 		delay_ms(1000);
 	}
 }
